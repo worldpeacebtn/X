73dd1ad61326e99a7587674c2ac130a088bdbd8e
Save the World now <224462947+worldpeacebtn@users.noreply.github.com>
2025-11-25 17:48:36 +0100

    Update HoloBackground.jsx

--- src/components/HoloBackground.jsx
+++ src/components/HoloBackground.jsx
@@ -6,13 +6,13 @@ export default function HoloBackground() {
   return (
     <Canvas className="absolute inset-0 w-full h-full !z-0" camera={{ position: [0, 0, 5] }}>
       <Stars
-        radius={92}
-        depth={35}
-        count={4200}
+        radius={120}
+        depth={50}
+        count={8000}
         factor={4}
         saturation={0}
         fade
-        speed={1.3}  // <--- make stars move
+        speed={1.5}
       />
       <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
     </Canvas>
