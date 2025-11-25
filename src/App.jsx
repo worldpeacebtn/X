dc3e8f244f0cb8dfbee804ba68d6361c2b443aaf
Save the World now <224462947+worldpeacebtn@users.noreply.github.com>
2025-11-25 17:59:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HoloBackground from "./components/HoloBackground";
+import NavBar from "./components/NavBar";
+
+import Auth from "./pages/Auth";
 import Vault from "./pages/Vault";
+import Upload from "./pages/Upload";
+import FileView from "./pages/FileView";
+import Share from "./pages/Share";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <Router>
-      <div className="relative min-h-screen w-full overflow-hidden">
-        <HoloBackground />
-        <div className="relative z-10 p-6 md:p-10">
-          <Routes>
-            <Route path="/" element={<Vault />} />
-          </Routes>
-        </div>
+    <div className="relative min-h-screen w-full overflow-hidden bg-[#050b1a] text-white">
+      <HoloBackground />
+      <NavBar />
+      <div className="relative z-10 p-6 md:p-10">
+        <Routes>
+          <Route path="/" element={<Vault />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/file/:id" element={<FileView />} />
+          <Route path="/share/:tag" element={<Share />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
