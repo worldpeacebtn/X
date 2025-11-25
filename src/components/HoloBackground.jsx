721e5186059a2e6065c813d916e915729a40867e
Save the World now <224462947+worldpeacebtn@users.noreply.github.com>
2025-11-25 17:38:07 +0100

    Update HoloBackground.jsx

--- src/components/HoloBackground.jsx
+++ src/components/HoloBackground.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-
 import { Canvas } from "@react-three/fiber";
 import { Stars, OrbitControls } from "@react-three/drei";
 
 export default function HoloBackground() {
   return (
     <Canvas className="absolute inset-0 w-full h-full !z-0" camera={{ position: [0, 0, 5] }}>
-      <Stars radius={120} depth={50} count={8000} factor={4} saturation={0} fade speed={1} />
+      <Stars
+        radius={92}
+        depth={35}
+        count={4200}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1.3}  // <--- make stars move
+      />
       <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
     </Canvas>
   );
