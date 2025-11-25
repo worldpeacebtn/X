0b824e5ae81fd73058f5d810bdd2a6233ab4a591
Save the World now <224462947+worldpeacebtn@users.noreply.github.com>
2025-11-25 16:54:19 +0100

    Create NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <nav className="relative z-20 w-full flex justify-center py-6">
+      <div className="bg-white/5 backdrop-blur-md px-6 py-3 rounded-full border border-white/10 flex gap-6">
+        <Link to="/">Vault</Link>
+        <Link to="/upload">Upload</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/auth">Login</Link>
+      </div>
+    </nav>
+  );
+}
