094238871af9cd6ab009b725bc0977ad4b420515
Save the World now <224462947+worldpeacebtn@users.noreply.github.com>
2025-11-25 18:09:34 +0100

    Update FileCard.jsx

--- src/components/FileCard.jsx
+++ src/components/FileCard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-export default function FileCard({ file }) {
+export default function HoloCard({ children }) {
   return (
-    <div className="card">
+    <div className="bg-white/5 border border-white/10 rounded-2xl p-6 shadow-lg backdrop-blur-xl hover:scale-105 transform transition-all duration-300">
+    
       <h2 className="text-lg font-semibold truncate">{file.name}</h2>
       <a
         href={`https://poiwwrnhyiztaadlpuiq/storage/v1/object/public/vault/${file.name}`}
