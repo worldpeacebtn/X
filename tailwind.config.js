c5a6c7c75c16ba67363bb3cfc61cb89b3bd7a9f0
Save the World now <224462947+worldpeacebtn@users.noreply.github.com>
2025-11-25 17:52:11 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        holoBlue: "#00ffff",
+        holoPurple: "#c700ff",
+        holoPink: "#ff007f",
+      },
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
